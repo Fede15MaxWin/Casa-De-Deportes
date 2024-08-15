797eec3798f1b2367cc7e16cfceb2cf05329a9af
Fede15MaxWin <federico46602@gmail.com>
2024-08-15 09:24:52 -0300

    creo repositorio

---
+++ global/config.php
@@ -0,0 +1,8 @@
+<?php
+define("KEY","minitienda");
+define("COD","AES-128-ECB");
+define("SERVIDOR","Localhosto");
+define("USUARIO","root");
+define("PASSWORD","");
+define("BD","tienda");
+?>
---
+++ mostrarCarrito.php
@@ -0,0 +1,17 @@
+<?php
+include 'global/config.php';
+include 'global/conexion.php';
+include 'carrito.php';
+include 'template/cabecera.php';
+?>
+<br>
+<h3>lista del carrito</h3>
+<table class="table table-light">
+    <tbody>
+        <tr>
+            <td>contenido del carrito...</td>
+        </tr>
+    </tbody>
+</table>
+
+<?php include 'template/cabecera.php'; ?>
